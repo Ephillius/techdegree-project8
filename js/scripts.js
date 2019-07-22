1446da3fdc1f26da69cf7ccc1faa3dc0f5269918
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-07-22 16:35:55 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ const directory = document.getElementById('directory');
 // -----------------------------
 
 $.ajax({
-	url: 'https://randomuser.me/api/?results=12',
+	url: 'https://randomuser.me/api/?results=12&nat=gb',
 	dataType: 'json',
 	success: function(data) {
 		const users = data.results;
@@ -14,20 +14,24 @@ $.ajax({
 		$.each(users, function(index, value) {
 			const name = users[index].name.first + ' ' + users[index].name.last;
 			const pic = users[index].picture.large;
-			const email = users.email;
+			const email = users[index].email;
 			const city = users[index].location.city;
-			const phone = users.phone;
+			const phone = users[index].phone;
 			const address = users[index].location.street + ' ' + users[index].location.state + ' ' + users[index].location.city + ' ' + users[index].location.postcode;
 			const birthday = users[index].dob.date;
 
 			const html = 
 			`
-				<div class="card">
-					<img src="${pic}" alt>
-					<h3 class="name">${name}</h3>
+				<div id="${index+1}" class="card">
+					<img class="photo" src="${pic}" alt>
+					<div class="text">
+						<h3 class="name">${name}</h3>
+						<p class="email">${email}</p>
+						<p class="city">${city}</p>
+					</div>
 				</div>
 			`;
-			directory.innerHTML = html;
+			directory.innerHTML += html;
 		});
 	}
 })
