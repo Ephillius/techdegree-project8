45feb8b9a1fca9f5c5ff3f23156f009a99d30e1b
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-07-27 07:21:07 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,78 @@
-const overlay = document.getElementById('overlay');
-overlay.style.display = "none";
-const modalCard = document.querySelector('.modal_card');
-const close = document.getElementById('close');
+window.onload = function () {
 
-const directory = document.getElementById('directory');
-const card = document.getElementsByClassName('card');
+	const input = document.querySelector('input').value;
+	const overlay = document.getElementById('overlay');
+	overlay.style.display = "none";
+	const modalCard = document.querySelector('.modal_card');
 
-
-// -----------------------------
-//  FETCH API
-// -----------------------------
-
-$.ajax({
-	url: 'https://randomuser.me/api/?results=12&nat=gb',
-	dataType: 'json',
-	success: function(data) {
-		const users = data.results;
-		console.log(users);
+	const directory = document.getElementById('directory');
+	const card = document.getElementsByClassName('card');
 
 
-		// -----------------------------
-		//  BUILD FUNCTION
-		// -----------------------------
+	// -----------------------------
+	//  FETCH API
+	// -----------------------------
 
-		$(users).each(function(index, value) {
-			let name = users[index].name.first;
-					name += ' ';
-					name += users[index].name.last;
-			const pic = users[index].picture.large;
-			const email = users[index].email;
-			const city = users[index].location.city;
-
-			const html = 
-			`
-				<div class="card">
-					<img class="photo" src="${pic}" alt>
-					<div class="text">
-						<h3 class="name">${name}</h3>
-						<p class="email">${email}</p>
-						<p class="city">${city}</p>
-					</div>
-				</div>
-			`;
-			directory.innerHTML += html;
-		});
+	$.ajax({
+		url: 'https://randomuser.me/api/?results=12&nat=gb',
+		dataType: 'json',
+		success: function(data) {
+			const users = data.results;
+			console.log(users);
 
 
-		// -----------------------------
-		//  MODAL
-		// -----------------------------		
+			// -----------------------------
+			//  BUILD FUNCTION
+			// -----------------------------
 
-		$(card && users).each(function(index) {
-			card[index].addEventListener('click', () => {
-				console.log(`Card nº${index+1} clicked.`);
-
+			$(users).each(function(index, value) {
 				let name = users[index].name.first;
 						name += ' ';
 						name += users[index].name.last;
 				const pic = users[index].picture.large;
 				const email = users[index].email;
 				const city = users[index].location.city;
-				const phone = users[index].phone;
-				let address = users[index].location.street;
-						address += ' - ';
-						address += users[index].location.state;
-						address += ' ';
-						address += users[index].location.city;
-						address += ', ';
-						address += users[index].location.postcode;
-				const birthday = users[index].dob.date;
+
 				const html = 
 				`
-					<div class="modal_card">
+					<div class="card">
+						<img class="photo" src="${pic}" alt>
+						<div class="text">
+							<h3 class="name">${name}</h3>
+							<p class="email">${email}</p>
+							<p class="city">${city}</p>
+						</div>
+					</div>
+				`;
+				directory.innerHTML += html;
+			});
+
+
+			// -----------------------------
+			//  MODAL
+			// -----------------------------		
+
+			$(card && users).each(function(index) {
+				card[index].addEventListener('click', () => {
+					console.log(`Card nº${index+1} clicked.`);
+
+					let name = users[index].name.first;
+							name += ' ';
+							name += users[index].name.last;
+					const pic = users[index].picture.large;
+					const email = users[index].email;
+					const city = users[index].location.city;
+					const phone = users[index].phone;
+					let address = users[index].location.street;
+							address += ' - ';
+							address += users[index].location.state;
+							address += ' ';
+							address += users[index].location.city;
+							address += ', ';
+							address += users[index].location.postcode;
+					const birthday = users[index].dob.date;
+					const html = 
+					`
 						<i id="close" class="fa fa-close"></i>
 						<img class="photo" src="${pic}" alt>
 						<div class="text">
@@ -83,25 +84,40 @@ $.ajax({
 							<p class="address">${address}</p>
 							<p class="birthday">${birthday}</p>
 						</div>
-					</div>
-				`;
-				overlay.style.display = "flex"; 
-				overlay.innerHTML += html;
-			});
+					`;
+					overlay.style.display = ''; 
+					modalCard.innerHTML = html;
+				});
 
-			modalCard.addEventListener('click', e => {
-				if(e.target === close) {
+
+				// -----------------------------
+				//  CLOSE MODAL
+				// -----------------------------				
+
+				overlay.addEventListener('click', e => {
+					if(e.target.id === "overlay" || e.target.id === "close") {
 						overlay.style.display = "none";
-				};
+					}
+				});
+
+
+				// -----------------------------
+				//  SEARCH USER
+				// -----------------------------
+				
+				let name = users[index].name.first;
+					name += ' ';
+					name += users[index].name.last;
+
+				const regexp = /w/g;
+				const match = name.match(regexp);
+
+				if(input !== match) {
+					card[index].style.display = '';
+				} else {
+					card[index].style.display = 'none';
+				}
 			});
-
-		});
-
-
-		// -----------------------------
-		//  SEARCH USER
-		// -----------------------------
-	
-
-	}
-})
+		}
+	});
+};
